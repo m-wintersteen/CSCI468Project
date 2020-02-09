0a90d0ed5621d53de21f9b67e57be51a55aad2b3
Jaret Boyer <jaret.c.boyer@gmail.com>
2020-02-08 17:41:36 -0700

    Created a lexer grammar file for step 1 and generated the LittleLexer class using ANTLR.

---
+++ src/antlr/Little.g4
@@ -0,0 +1,9 @@
+lexer grammar Little;
+
+KEYWORD : 'PROGRAM' | 'BEGIN' | 'END' | 'FUNCTION' | 'READ' | 'WRITE' | 'IF' | 'ELSE' | 'ENDIF' | 'WHILE' | 'ENDWHILE' | 'CONTINUE' | 'BREAK' | 'RETURN' | 'INT' | 'VOID' | 'STRING' | 'FLOAT' ;
+IDENTIFIER : [A-z]+([A-z]|[0-9])* ;
+OPERATOR : ':=' | '+' | '-' | '*' | '/' | '=' | '!=' | '<' | '>' | '(' | ')' | ';' | ',' | '<=' | '>=' ;
+INTLITERAL : [0-9]+ ;
+FLOATLITERAL: [0-9]*.[0-9]+ ;
+STRINGLITERAL: '"'[^"]*'"' ;
+COMMENT: '--'.*?'\n' ;
---
+++ src/antlr/LittleLexer.py
@@ -0,0 +1,117 @@
+# Generated from Little.g4 by ANTLR 4.8
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\t")
+        buf.write("\u00ac\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3")
+        buf.write("\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3")
+        buf.write("\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3")
+        buf.write("\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3")
+        buf.write("\2\3\2\5\2o\n\2\3\3\6\3r\n\3\r\3\16\3s\3\3\7\3w\n\3\f")
+        buf.write("\3\16\3z\13\3\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\5\4\u0086\n\4\3\5\6\5\u0089\n\5\r\5\16\5\u008a\3\6\7")
+        buf.write("\6\u008e\n\6\f\6\16\6\u0091\13\6\3\6\3\6\6\6\u0095\n\6")
+        buf.write("\r\6\16\6\u0096\3\7\3\7\7\7\u009b\n\7\f\7\16\7\u009e\13")
+        buf.write("\7\3\7\3\7\3\b\3\b\3\b\3\b\7\b\u00a6\n\b\f\b\16\b\u00a9")
+        buf.write("\13\b\3\b\3\b\3\u00a7\2\t\3\3\5\4\7\5\t\6\13\7\r\b\17")
+        buf.write("\t\3\2\b\3\2C|\4\2\62;C|\6\2,-//\61\61??\6\2*+..=>@@\3")
+        buf.write("\2\62;\4\2$$``\2\u00c8\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2")
+        buf.write("\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2")
+        buf.write("\3n\3\2\2\2\5q\3\2\2\2\7\u0085\3\2\2\2\t\u0088\3\2\2\2")
+        buf.write("\13\u008f\3\2\2\2\r\u0098\3\2\2\2\17\u00a1\3\2\2\2\21")
+        buf.write("\22\7R\2\2\22\23\7T\2\2\23\24\7Q\2\2\24\25\7I\2\2\25\26")
+        buf.write("\7T\2\2\26\27\7C\2\2\27o\7O\2\2\30\31\7D\2\2\31\32\7G")
+        buf.write("\2\2\32\33\7I\2\2\33\34\7K\2\2\34o\7P\2\2\35\36\7G\2\2")
+        buf.write("\36\37\7P\2\2\37o\7F\2\2 !\7H\2\2!\"\7W\2\2\"#\7P\2\2")
+        buf.write("#$\7E\2\2$%\7V\2\2%&\7K\2\2&\'\7Q\2\2\'o\7P\2\2()\7T\2")
+        buf.write("\2)*\7G\2\2*+\7C\2\2+o\7F\2\2,-\7Y\2\2-.\7T\2\2./\7K\2")
+        buf.write("\2/\60\7V\2\2\60o\7G\2\2\61\62\7K\2\2\62o\7H\2\2\63\64")
+        buf.write("\7G\2\2\64\65\7N\2\2\65\66\7U\2\2\66o\7G\2\2\678\7G\2")
+        buf.write("\289\7P\2\29:\7F\2\2:;\7K\2\2;o\7H\2\2<=\7Y\2\2=>\7J\2")
+        buf.write("\2>?\7K\2\2?@\7N\2\2@o\7G\2\2AB\7G\2\2BC\7P\2\2CD\7F\2")
+        buf.write("\2DE\7Y\2\2EF\7J\2\2FG\7K\2\2GH\7N\2\2Ho\7G\2\2IJ\7E\2")
+        buf.write("\2JK\7Q\2\2KL\7P\2\2LM\7V\2\2MN\7K\2\2NO\7P\2\2OP\7W\2")
+        buf.write("\2Po\7G\2\2QR\7D\2\2RS\7T\2\2ST\7G\2\2TU\7C\2\2Uo\7M\2")
+        buf.write("\2VW\7T\2\2WX\7G\2\2XY\7V\2\2YZ\7W\2\2Z[\7T\2\2[o\7P\2")
+        buf.write("\2\\]\7K\2\2]^\7P\2\2^o\7V\2\2_`\7X\2\2`a\7Q\2\2ab\7K")
+        buf.write("\2\2bo\7F\2\2cd\7U\2\2de\7V\2\2ef\7T\2\2fg\7K\2\2gh\7")
+        buf.write("P\2\2ho\7I\2\2ij\7H\2\2jk\7N\2\2kl\7Q\2\2lm\7C\2\2mo\7")
+        buf.write("V\2\2n\21\3\2\2\2n\30\3\2\2\2n\35\3\2\2\2n \3\2\2\2n(")
+        buf.write("\3\2\2\2n,\3\2\2\2n\61\3\2\2\2n\63\3\2\2\2n\67\3\2\2\2")
+        buf.write("n<\3\2\2\2nA\3\2\2\2nI\3\2\2\2nQ\3\2\2\2nV\3\2\2\2n\\")
+        buf.write("\3\2\2\2n_\3\2\2\2nc\3\2\2\2ni\3\2\2\2o\4\3\2\2\2pr\t")
+        buf.write("\2\2\2qp\3\2\2\2rs\3\2\2\2sq\3\2\2\2st\3\2\2\2tx\3\2\2")
+        buf.write("\2uw\t\3\2\2vu\3\2\2\2wz\3\2\2\2xv\3\2\2\2xy\3\2\2\2y")
+        buf.write("\6\3\2\2\2zx\3\2\2\2{|\7<\2\2|\u0086\7?\2\2}\u0086\t\4")
+        buf.write("\2\2~\177\7#\2\2\177\u0086\7?\2\2\u0080\u0086\t\5\2\2")
+        buf.write("\u0081\u0082\7>\2\2\u0082\u0086\7?\2\2\u0083\u0084\7@")
+        buf.write("\2\2\u0084\u0086\7?\2\2\u0085{\3\2\2\2\u0085}\3\2\2\2")
+        buf.write("\u0085~\3\2\2\2\u0085\u0080\3\2\2\2\u0085\u0081\3\2\2")
+        buf.write("\2\u0085\u0083\3\2\2\2\u0086\b\3\2\2\2\u0087\u0089\t\6")
+        buf.write("\2\2\u0088\u0087\3\2\2\2\u0089\u008a\3\2\2\2\u008a\u0088")
+        buf.write("\3\2\2\2\u008a\u008b\3\2\2\2\u008b\n\3\2\2\2\u008c\u008e")
+        buf.write("\t\6\2\2\u008d\u008c\3\2\2\2\u008e\u0091\3\2\2\2\u008f")
+        buf.write("\u008d\3\2\2\2\u008f\u0090\3\2\2\2\u0090\u0092\3\2\2\2")
+        buf.write("\u0091\u008f\3\2\2\2\u0092\u0094\13\2\2\2\u0093\u0095")
+        buf.write("\t\6\2\2\u0094\u0093\3\2\2\2\u0095\u0096\3\2\2\2\u0096")
+        buf.write("\u0094\3\2\2\2\u0096\u0097\3\2\2\2\u0097\f\3\2\2\2\u0098")
+        buf.write("\u009c\7$\2\2\u0099\u009b\t\7\2\2\u009a\u0099\3\2\2\2")
+        buf.write("\u009b\u009e\3\2\2\2\u009c\u009a\3\2\2\2\u009c\u009d\3")
+        buf.write("\2\2\2\u009d\u009f\3\2\2\2\u009e\u009c\3\2\2\2\u009f\u00a0")
+        buf.write("\7$\2\2\u00a0\16\3\2\2\2\u00a1\u00a2\7/\2\2\u00a2\u00a3")
+        buf.write("\7/\2\2\u00a3\u00a7\3\2\2\2\u00a4\u00a6\13\2\2\2\u00a5")
+        buf.write("\u00a4\3\2\2\2\u00a6\u00a9\3\2\2\2\u00a7\u00a8\3\2\2\2")
+        buf.write("\u00a7\u00a5\3\2\2\2\u00a8\u00aa\3\2\2\2\u00a9\u00a7\3")
+        buf.write("\2\2\2\u00aa\u00ab\7\f\2\2\u00ab\20\3\2\2\2\r\2nsvx\u0085")
+        buf.write("\u008a\u008f\u0096\u009c\u00a7\2")
+        return buf.getvalue()
+
+
+class LittleLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    KEYWORD = 1
+    IDENTIFIER = 2
+    OPERATOR = 3
+    INTLITERAL = 4
+    FLOATLITERAL = 5
+    STRINGLITERAL = 6
+    COMMENT = 7
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+ ]
+
+    symbolicNames = [ "<INVALID>",
+            "KEYWORD", "IDENTIFIER", "OPERATOR", "INTLITERAL", "FLOATLITERAL", 
+            "STRINGLITERAL", "COMMENT" ]
+
+    ruleNames = [ "KEYWORD", "IDENTIFIER", "OPERATOR", "INTLITERAL", "FLOATLITERAL", 
+                  "STRINGLITERAL", "COMMENT" ]
+
+    grammarFileName = "Little.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.8")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
