48b3430a314a79fb80ca4205ae701982261fd32e
Jaret Boyer <jaret.c.boyer@gmail.com>
2020-04-10 22:29:39 -0600

    finished ir to tiny conversion

--- src/step4/Translator.py
+++ src/step4/Translator.py
@@ -1,3 +1,5 @@
+import re
+
 
 class Translator:
 
@@ -5,40 +7,35 @@ class Translator:
         self.r = -1
 
 
-    def getRegister(self):
-        self.r = self.r + 1
-        return 'r{}'.format(self.r)
+    def toreg(self, temp):
+        matches = re.match(r'\$T(\d+)', temp)
+        if matches:
+            return 'r{}'.format(int(matches.group(1)) - 1)
+        else:
+            return temp
 
 
     def translate(self, code, symbolTable):
         assembly = []
         regmap = {}
         for id in symbolTable:
-            assembly.append('var {}'.format(id))
+            if symbolTable[id]['type'] == 'STRING':
+                assembly.append('str {} {}'.format(id, symbolTable[id]['value']))
+            else:
+                assembly.append('var {}'.format(id))
         for line in code:
             [instr, *args] = line.split(' ')
             if len(args) == 3:
-                pass
-                # op1 = Executor.registers[args[0]]
-                # op2 = Executor.registers[args[1]]
-                # result = None
-                # if (instr == 'ADDI'):
-                #     result = op1 + op2
-                # elif (instr == 'SUBI'):
-                #     result = op1 - op2
-                # elif (instr == 'MULTI'):
-                #     result = op1 * op2
-                # elif (instr == 'DIVI'):
-                #     result = op1 / op2
-                # Executor.registers[args[2]] = result
+                op1 = self.toreg(args[0])
+                op2 = self.toreg(args[1])
+                dest = self.toreg(args[2])
+                assembly.append('move {} {}'.format(op1, dest))
+                assembly.append('{} {} {}'.format(instr.lower().replace('f', 'r').replace('t', ''), op2, dest))
             elif len(args) == 2:
-                if args[1][0] == '$':
-                    regmap[args[1]] = self.getRegister
-                    assembly.append('move {} {}'.format(args[0], regmap[args[1]]))
-                else:
-                    assembly.append('move {} {}'.format(regmap[args[0]], args[1]))
+                assembly.append('move {} {}'.format(self.toreg(args[0]), self.toreg(args[1])))
             else:
-                pass
+                assembly.append('sys {} {}'.format(instr.lower().replace('f', 'r'), args[0]))
+        assembly.append('sys halt')
 
         for line in assembly:
             print(line)
